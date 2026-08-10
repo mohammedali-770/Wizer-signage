3b9b8dba1f3e51a7d355bea6f242ed8ac0df9d30
mohammedali-770 <mohammed.ali@1sttaste.com>
2026-08-10 11:01:26 +0300

    feat(reports): register manual run queue

--- apps/api/src/modules/scheduled-reports/scheduled-reports.module.ts
+++ apps/api/src/modules/scheduled-reports/scheduled-reports.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common';
 import { ActivityLogModule } from '../activity-log/activity-log.module';
 import { ExportsModule } from '../exports/exports.module';
 import { NotificationsModule } from '../notifications/notifications.module';
+import { ScheduledReportQueueService } from './scheduled-report-queue.service';
 import { ScheduledReportService } from './scheduled-report.service';
 import { ScheduledReportsController } from './scheduled-reports.controller';
 
@@ -14,7 +15,7 @@ import { ScheduledReportsController } from './scheduled-reports.controller';
 @Module({
   imports: [ActivityLogModule, ExportsModule, NotificationsModule],
   controllers: [ScheduledReportsController],
-  providers: [ScheduledReportService],
-  exports: [ScheduledReportService],
+  providers: [ScheduledReportService, ScheduledReportQueueService],
+  exports: [ScheduledReportService, ScheduledReportQueueService],
 })
 export class ScheduledReportsModule {}
